03f7253aeb16fd1b0d44fd6837e1921790ade319
Austin.Kucera01 <austin.kucera01@cardinalhealth.com>
2022-08-12 11:14:28 -0400

    full flow

--- src/main/kotlin/Client.kt
+++ src/main/kotlin/Client.kt
@@ -6,9 +6,12 @@ import kotlinx.browser.document
 import kotlinx.browser.window
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
+import kotlinx.html.BODY
 import kotlinx.html.InputType
+import kotlinx.html.TagConsumer
 import kotlinx.html.body
 import kotlinx.html.br
+import kotlinx.html.classes
 import kotlinx.html.div
 import kotlinx.html.dom.append
 import kotlinx.html.form
@@ -24,6 +27,7 @@ import org.w3c.dom.Node
 import org.w3c.dom.get
 import kotlinx.serialization.*
 import kotlinx.serialization.json.*
+import org.w3c.dom.HTMLElement
 import org.w3c.dom.events.Event
 
 val client = HttpClient(Js)
@@ -46,54 +50,60 @@ fun Node.sayHello() {
                 br { }
                 +"Please fill out this quick form so we can set up your account."
             }
-            div {
-                form {
-                    id = "signup-form"
-                    div("form-line") {
-                        label { +"Email" }
-                        input(InputType.email) {
-                            id = "email"
-                            required = true
-                            value = "robert.kucera@cardinalhealth.com"
-                        }
-                    }
+            buildSignupForm()
 
-                    div("form-line") {
-                        label { +"Member Id" }
-                        input(InputType.text) {
-                            id = "member-id"
-                            required = true
-                            value = "123"
-                        }
-                    }
-
-                    div("form-line") {
-                        label { +"First Name" }
-                        input(InputType.text) {
-                            id = "first-name"
-                            required = true
-                            value = "Bob"
-                        }
-                    }
-
-                    div("form-line") {
-                        label { +"Last Name" }
-                        input(InputType.text) {
-                            id = "last-name"
-                            required = true
-                            value = "Baggins"
-                        }
-                    }
-
-                    submitInput {
-                        id = "submit-signup"
-                    }
-                    onSubmitFunction = ::submitSignup
-                }
-            }
             div {
                 id = "submit-status"
             }
+        }
+    }
+}
+
+private fun BODY.buildSignupForm() {
+    div {
+        id = "signup-wrapper"
+        form {
+            id = "signup-form"
+            div("form-line") {
+                label { +"Email" }
+                input(InputType.email) {
+                    id = "email"
+                    required = true
+                    value = "robert.kucera@cardinalhealth.com"
+                }
+            }
+
+            div("form-line") {
+                label { +"Member Id" }
+                input(InputType.text) {
+                    id = "member-id"
+                    required = true
+                    value = "123"
+                }
+            }
+
+            div("form-line") {
+                label { +"First Name" }
+                input(InputType.text) {
+                    id = "first-name"
+                    required = true
+                    value = "Bob"
+                }
+            }
+
+            div("form-line") {
+                label { +"Last Name" }
+                input(InputType.text) {
+                    id = "last-name"
+                    required = true
+                    value = "Baggins"
+                }
+            }
+
+            submitInput {
+                classes = setOf("submit")
+            }
+            onSubmitFunction = ::submitSignup
         }
     }
 }
@@ -119,16 +129,69 @@ private fun submitSignup(event: Event) {
         }
         if (response.status == HttpStatusCode.OK) {
             println("Enrollment started")
+            document.getElementById("signup-wrapper")?.innerHTML = ""
             submitStatus.append {
                 div("submit-success") {
-                    +"Enrollment started. Check your email!"
+                    +"Enrollment started. Enter the code from your email!"
                 }
+                buildEmailForm()
             }
         } else {
             println("Enrollment Failed")
             submitStatus.append {
                 div("submit-failed") {
                     +"Enrollment Failed. Please try again"
+                }
+            }
+        }
+    }
+}
+
+fun TagConsumer<HTMLElement>.buildEmailForm() {
+    div {
+        id = "email-form-wrapper"
+        br { }
+        form {
+            id = "email-code-form"
+            div("form-line") {
+                label { +"Code" }
+                input(InputType.text) {
+                    id = "code"
+                    required = true
+                    value = ""
+                }
+            }
+            submitInput {
+                id = "submit-email-code"
+                classes = setOf("submit")
+            }
+            onSubmitFunction = ::submitVerification
+        }
+    }
+}
+
+private fun submitVerification(event: Event) {
+    event.preventDefault()
+    val submitStatus = document.getElementById("submit-status")!!
+    val form = document.getElementById("email-code-form") as HTMLFormElement
+    val code = (form["code"] as HTMLInputElement).value
+    GlobalScope.launch {
+        val response = client.post("http://localhost:8080/enroll/$code")
+        if (response.status == HttpStatusCode.OK) {
+            println("Enrollment Complete")
+            document.getElementById("email-form-wrapper")?.innerHTML = ""
+            document.getElementById("submit-status")?.innerHTML = ""
+            submitStatus.append {
+                div("submit-success") {
+                    +"Enrollment Complete!"
+                }
+
+            }
+        } else {
+            println("Enrollment Failed")
+            submitStatus.append {
+                div("submit-failed") {
+                    +"Enrollment Failed. Please try again."
                 }
             }
         }
